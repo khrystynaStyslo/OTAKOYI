1fa39887f2c7e7bfbcba7297bebdeafe75455942
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-11-02 22:31:09 +0200

    Updates

---
+++ precache-manifest.7230c3be5a7fb0616eefd57be86f85c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5be277c617e5ec72766611297619c4",
+    "url": "/OTAKOYI/index.html"
+  },
+  {
+    "revision": "d833a7a09b8edc88ecdf",
+    "url": "/OTAKOYI/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "8c1ab1ba44e6b601937b",
+    "url": "/OTAKOYI/static/css/main.9a7169f8.chunk.css"
+  },
+  {
+    "revision": "d833a7a09b8edc88ecdf",
+    "url": "/OTAKOYI/static/js/2.cefd25b8.chunk.js"
+  },
+  {
+    "revision": "8c1ab1ba44e6b601937b",
+    "url": "/OTAKOYI/static/js/main.3df537c7.chunk.js"
+  },
+  {
+    "revision": "641cd5ed814a17d4a3c9",
+    "url": "/OTAKOYI/static/js/runtime~main.c905b6df.js"
+  }
+]);
